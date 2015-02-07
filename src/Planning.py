8d437d3d6922220365320af197d419fe84924ccc
rahlk <i.m.ralk@gmail.com>
2015-02-06 22:32:00 -0500

    More changes

--- src/Planning.py
+++ src/Planning.py
@@ -38,20 +38,30 @@ import sk
 #>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 class deltas():
 
-  def __init__(self, row):
-    self.row = row;
+  def __init__(self, row, myTree):
+    self.loc = drop(row, myTree)
     self.contrastSet = None
-
-  def patchUp(self, keys):
-
-    pass
+    self.newRow = row;
+    self.score = self.scorer(self.loc)
+  def scorer(self, node):
+    return mean([r.cells[-2] for r in node.rows])
+  def createNew(self, low, hi, N = 1):
+    for _ in xrange(N):
+      new = max(lo, min(hi, lo + rand() * abs(hi - lo)))
+  def applyPatch(self, keys):
+    for stuff in self.contrastSet:
+      lo, hi = stuff[1]
+      pos = keys[stuff[0].name]
+      self.newRow.cells[pos] = createNew(lo, hi)
+    return self.newRow
 
 class treatments():
   "Treatments"
   def __init__(self, train = None, test = None,
                verbose = True, smoteit = False):
-    self.train, self.test = train, test
+    self.train_DF, self.test_DF = createTbl(train), createTbl(test)
     self.verbose, self.smoteit = verbose, smoteit
+    self.mod, self.keys = [], self.getKey()
 
   def leaves(self, node):
     L = []
@@ -64,17 +74,16 @@ class treatments():
     else:
       return node
 
+  def scorer(self, node):
+    return mean([r.cells[-2] for r in node.rows])
+
   def isBetter(self, me, others):
     some1 = False
     for notme in others:
-      if self.score(notme) < self.score(me):
+      if self.scorer(notme) < self.scorer(me):
         return False, notme.branch  # False here is for the 'notFound' variable
       else:
         return True, []
-
-  def score(self, node):
-    return mean([r.cells[-2] for r in node.rows])
-
 
   def finder(self, node):
     notFound = True; oldNode = []
@@ -89,6 +98,11 @@ class treatments():
         oldNode.append(node)
     return branch
 
+  def getKey(self):
+    keys = {}
+    for i in xrange(len(self.test_DF.headers)):
+      keys.update({self.test_DF.headers[i].name[1:]:i})
+    return keys
 
   def main(self):
     #>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
@@ -96,34 +110,31 @@ class treatments():
     #>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
     # Training data
-    train_DF = createTbl(self.train)
     if self.smoteit:
-      train_DF = SMOTE(data = train_DF, atleast = 50, atmost = 100)
-
-    # Testing data
-    test_DF = createTbl(self.test)
+      self.train_DF = SMOTE(data = self.train_DF, atleast = 50, atmost = 100)
 
     # Decision Tree
-    t = discreteNums(train_DF, map(lambda x: x.cells, train_DF._rows))
+    t = discreteNums(self.train_DF, map(lambda x: x.cells, self.train_DF._rows))
     myTree = tdiv(t)
     if self.verbose: showTdiv(myTree)
 
     # Testing data
-    testCase = test_DF._rows
+    testCase = self.test_DF._rows
     newTab = []
     for tC in testCase:
       newRow = tC;
-      loc = drop(tC, myTree)
-      node = deltas(loc)  # A delta instance for the rows
-      if self.score(node.row) == 0:
+      node = deltas(newRow, myTree)  # A delta instance for the rows
+      if node.score == 0:
         print('No Contrast Set.')
         node.contrastSet = []
-        continue
+        self.mod.append(node.newRow)
       else:
         print('Obtaining contrast set. .. ...')
-        node.contrastSet = self.finder(node.row)
+        node.contrastSet = self.finder(node.loc)
+        self.mod.append(node.applyPatch(self.keys))
       print(node.__dict__)
-      set_trace()
+
+    return clone(self.test_DF, rows = self.mod, discrete = True)
 
 def planningTest():
   # Test contrast sets
