dfc7f4cc1b5249a49e29be67e393521836661c08
rahlk <i.m.ralk@gmail.com>
2015-03-17 09:26:17 -0400

    Log2Tex

--- SOURCE/log2tex.py
+++ SOURCE/log2tex.py
@@ -14,29 +14,73 @@ sys.path.extend([axe, pystat, cwd])
 from sk import rdivDemo
 from os import walk
 
-def striplines(line):
-  lists = []
-  listedline = line[1:-1].strip().split(',') # split around the = sign
-  lists.append(listedline[0][1:-1])
-  for ll in listedline[1:]:
-    lists.append(float(ll))
-  return lists
 
-def list2sk(lst):
-  return rdivDemo(lst, isLatex = True)
+class type1():
 
-def log2list():
-  lst = []
-  dir = './log'
-  files = [filenames for (dirpath, dirnames, filenames) in walk(dir)][0]
-  for file in files:
-    f = open(dir+'/'+file, 'r')
-    for line in f:
-      lst.append(striplines(line[:-1]))
-  print(list2sk(lst))
-  
+  def __init__(self):
+    pass
+
+  def striplines(self, line):
+    lists = []
+    listedline = line[1:-1].strip().split(',')  # split around the = sign
+    lists.append(listedline[0][1:-1])
+    for ll in listedline[1:]:
+      lists.append(float(ll))
+    return lists
+
+  def list2sk(self, lst):
+    return rdivDemo(lst, isLatex=True)
+
+  def log2list(self):
+    lst = []
+    dir = './log'
+    files = [filenames for (dirpath, dirnames, filenames) in walk(dir)][0]
+    for file in files:
+      f = open(dir + '/' + file, 'r')
+      for line in f:
+        lst.append(self.striplines(line[:-1]))
+    print(self.list2sk(lst))
+
+
+class type2():
+
+  def __init__(self):
+    pass
+
+  def striplines(self, line):
+    lists = []
+    listedline = line[1:-1].strip().split(',')  # split around the = sign
+    lists.append(listedline[0][1:-1])
+    for ll in listedline[1:]:
+      lists.append(float(ll))
+    return lists
+
+  def list2sk(self, lst):
+    return rdivDemo(lst, isLatex=True)
+
+  def log2list(self):
+    dir = './log'
+    files = [filenames for (
+        dirpath,
+        dirnames,
+        filenames) in walk(dir)][1:]
+    dirs = [dirpath for (
+        dirpath,
+        dirnames,
+        filenames) in walk(dir)][1:]
+#     set_trace()
+    for project, folder in zip(files, dirs):
+      lst = []
+#       set_trace()
+      for file in project:
+        f = open(folder + '/' + file, 'r')
+        for line in f:
+          lst.append(self.striplines(line[:-1]))
+      print(self.list2sk(lst))
+
+
 def _test():
-  log2list()
-  
+  type2().log2list()
+
 if __name__ == '__main__':
   _test()
