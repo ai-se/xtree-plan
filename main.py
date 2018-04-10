625c058c5a945c2db5c2122fe4893e34c5c23583
rahlk <i.m.ralk@gmail.com>
2018-04-10 17:45:57 -0400

    Autocommit

--- main.py
+++ main.py
@@ -31,26 +31,29 @@ def effectiveness(dframe):
     overlap = dframe['Overlap']
     heeded = dframe['Heeded']
 
-    a, b, c, d = 0,0,0,0
+    a, b, c, d, e, f = 0, 0, 0, 0, 0, 0
 
     for over, heed in zip(overlap, heeded):
-        if over <= 50 and heed >= 0:
+        if over < 50 and heed >= 0:
             a += 1
-        if over >= 50 and heed >= 0:
+        if over > 50 and heed >= 0:
             b += 1
-        if over <= 50 and heed < 0:
+        if over < 50 and heed < 0:
             c += 1
-        if over >= 50 and heed < 0:
+        if over > 50 and heed < 0:
             d += 1
-    
-    return a,b,c,d 
+        if over == 0 and heed < 0:
+            e += 1
+        if over == 0 and heed >= 0:
+            f += 1
 
+    return a, b, c, d, e, f
 
 
 def planning():
     data = get_all_projects()
-    results = dict()
-    print("Name, Overlap<50 | Bugs Reduced, Overlap > 50 | Bugs Reduced, Overlap<50 | Bugs Increased , Overlap>50 | Bugs Increased, ")
+    file = open(os.path.join(os.path.realpath(root), "results", "results.csv"), "w+")
+    print("Name, Overlap<50 | Bugs Reduced, Overlap > 50 | Bugs Reduced, Overlap<50 | Bugs Increased , Overlap>50 | Bugs Increased, No Overlap | Bugs Increased, No Overlap | Bugs Decreased", file=file)
     for proj, paths in data.iteritems():
         # heeded = []
         i = 0
@@ -66,44 +69,55 @@ def planning():
             "Recommend changes with XTREE"
             new = xtree(train[train.columns[1:]], test)
 
+            "Find modules that appear both in test and validation datasets"
             common_modules = pd.merge(
                 test, validation, how='inner', on=['Name'])['Name']
 
+            "Intitialize variables to hold information"
             improve_heeded = []
             overlap = []
 
             for module_name in common_modules:
-
-                heeded = 0
-
-                test_value = test.loc[test['Name'] == module_name]
-                plan_value = new.loc[new['Name'] == module_name]
-                validate_value = validation.loc[validation['Name']
+                "For every common class, do the following ... "
+                same = 0 # Keep track of features that follow XTREE's recommendations
+                test_value = test.loc[test['Name'] == module_name] # Metric values of classes in the test set
+                plan_value = new.loc[new['Name'] == module_name] # Metric values of classes in the XTREE's planned changes
+                validate_value = validation.loc[validation['Name'] # Actual metric values the developer's changes yielded
                                                 == module_name]
+                
 
                 for col in test_value.columns[1:-1]:
+                    "For every metric (except the class name and the bugs), do the following ... "
                     try:
                         if isinstance(plan_value[col].values[0], str):
+                            "If the change recommended by XTREE lie's in a range of values"
                             if eval(plan_value[col].values[0])[0] <= validate_value[col].values[0] <= eval(plan_value[col].values[0])[1]:
-                                heeded += 1
+                                "If the actual change lies withing the recommended change, then increment the count of heeded values by 1"
+                                same += 1
                         elif plan_value[col].values[0] == validate_value[col].values[0]:
-                                heeded += 1
+                                "If the XTREE recommends no change and developers didn't change anything, that also counts as an overlap"
+                                same += 1
 
                     except IndexError:
                         pass
 
-                overlap.append(int(heeded * 5))
+                overlap.append(int(same/20 * 100)) # There are 20 metrics, so, find % of overlap for the class.
                 heeded = test_value['bug'].values[0] - \
-                    validate_value['bug'].values[0]
-                scale = -1 if heeded <= 0 else 1
-                improve_heeded.append(0.5*random()*scale+heeded)
+                    validate_value['bug'].values[0] # Find the change in the number of bugs between the test version and the validation version for that class.
+                
+                scale = -1 if heeded <= 0 else 1 # Used to add a random jiggle for the scatter plot.
+                
+                improve_heeded.append(0.5 * random() * scale + heeded) 
+
+            "Save the results ... "
 
             results['Overlap'] = overlap
             results['Heeded'] = improve_heeded
-
-            print("{}_{},{},{},{},{}".format(proj, i, *effectiveness(results)))
-            results.to_csv(
-                "results/{}/{}_{}.csv".format(proj, proj, i), index=False)
+            
+            file = open(os.path.join(os.path.realpath(root), "results", "results.csv"), "a")
+            print("{}_{},{},{},{},{},{},{}".format(
+                proj, i, *effectiveness(results)), file=file)
+            results.to_csv(os.path.join(os.path.realpath(root), "results", "{}/{}_{}.csv".format(proj, proj, i)), index=False)
 
 
 if __name__ == "__main__":
