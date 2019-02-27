7a15f145cfcd588823a3bbd0b5583a95af5d4e28
rahlk <i.m.ralk@gmail.com>
2019-02-27 13:05:45 -0500

    New Images

--- src/utils/stats_utils/auec.py
+++ src/utils/stats_utils/auec.py
@@ -1,10 +1,9 @@
 import warnings
 warnings.filterwarnings('ignore')
 from sklearn.metrics import auc
-
+from pdb import set_trace
 
 def compute_auec(dframe, y_max, y_min):
-
     x = dframe[dframe.columns[0]]
     y = dframe[dframe.columns[1]]
     [overlap_max, _] = dframe.max(axis=0).values
